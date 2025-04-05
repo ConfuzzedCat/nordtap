25ef614b0fb9f8bf09392312d470e197d79b1b83
ConfuzzedCat <confuzzedcat@gmail.com>
2025-04-05 19:38:11 +0200

    Added gitignore files and removed all frontend files

---
+++ backend/Extensions/Models/InfoRequest.cs
@@ -0,0 +1,24 @@
+namespace backend.Extensions.Models;
+
+/// <summary>
+/// The request type for the "/manage/info" endpoint added by <see cref="IdentityApiEndpointRouteBuilderExtensions.MapIdentityApiCustom"/>.
+/// All properties are optional. No modifications will be made to the user if all the properties are omitted from the request.
+/// </summary>
+public sealed class InfoRequest
+{
+    /// <summary>
+    /// The optional new username for the authenticated user. This will replace the username if there was one.
+    /// </summary>
+    public string? NewUsername { get; init; }
+
+    /// <summary>
+    /// The optional new password for the authenticated user. If a new password is provided, the <see cref="OldPassword"/> is required.
+    /// If the user forgot the old password, use the "/forgotPassword" endpoint instead.
+    /// </summary>
+    public string? NewPassword { get; init; }
+
+    /// <summary>
+    /// The old password for the authenticated user. This is only required if a <see cref="NewPassword"/> is provided.
+    /// </summary>
+    public string? OldPassword { get; init; }
+}

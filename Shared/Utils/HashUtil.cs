a8f2c32ce09426e785c2f3b52b33150535a4e478
ConfuzzedCat <confuzzedcat@gmail.com>
2025-08-28 22:30:34 +0200

    Changed impl of HashUtil method and made the class static

--- Shared/Utils/HashUtil.cs
+++ Shared/Utils/HashUtil.cs
@@ -3,18 +3,11 @@ using System.Text;
 
 namespace Shared.Utils;
 
-public class HashUtil
+public static class HashUtil
 {
     public static string Sha256String(string value)
     {
-        var sb = new StringBuilder();
-        var enc = Encoding.UTF8;
-        var result = SHA256.HashData(enc.GetBytes(value));
-
-        foreach (var b in result)
-        {
-            sb.Append(b.ToString("x2"));
-        }
-        return sb.ToString();
+        var byteArray = SHA256.HashData(Encoding.UTF8.GetBytes(value));
+        return Convert.ToHexString(byteArray);
     }
 }
